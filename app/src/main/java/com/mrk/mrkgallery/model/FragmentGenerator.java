89f88e692ea3291a819a109245407f23debe4c11
mrk <1152574267@qq.com>
2018-07-19 10:59:42 +0800

    HW AI Image label detect

---
+++ app/src/main/java/com/mrk/mrkgallery/model/FragmentGenerator.java
@@ -0,0 +1,23 @@
+package com.mrk.mrkgallery.model;
+
+import android.support.v4.app.Fragment;
+
+import com.mrk.mrkgallery.R;
+import com.mrk.mrkgallery.view.PhotoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentGenerator {
+    public static int[] strArr = new int[]{R.string.tv_image_label_detect, R.string.tv_123};
+    public static int[] drawableArr = new int[]{R.drawable.ic_launcher, R.drawable.ic_launcher};
+
+    public static List<Fragment> getFragmentList() {
+        List<Fragment> fragmentList = new ArrayList<Fragment>();
+        fragmentList.add(new PhotoFragment());
+        fragmentList.add(new PhotoFragment());
+
+        return fragmentList;
+    }
+
+}
---
+++ app/src/main/java/com/mrk/mrkgallery/view/PhotoFragment.java
@@ -0,0 +1,123 @@
+package com.mrk.mrkgallery.view;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.mrk.mrkgallery.R;
+import com.mrk.mrkgallery.adapter.MRecyclerViewAdapter;
+import com.mrk.mrkgallery.bean.PhotoItem;
+import com.mrk.mrkgallery.decoration.MyDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoFragment extends Fragment implements
+        MRecyclerViewAdapter.OnItemClickListener, MRecyclerViewAdapter.OnItemLongClickListener {
+    private static final String TAG = PhotoFragment.class.getSimpleName();
+
+    private Context mContext;
+    private RecyclerView photoList;
+    private MRecyclerViewAdapter<PhotoItem> mAdapter;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        List<PhotoItem> mPhotoList = new ArrayList<PhotoItem>();
+        mAdapter = new MRecyclerViewAdapter<PhotoItem>(mContext, mPhotoList);
+        mAdapter.setOnItemClickListener(this);
+        mAdapter.setOnItemLongClickListener(this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        Log.d(TAG, "onCreateView");
+
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated");
+
+        photoList = (RecyclerView) view.findViewById(R.id.tablist);
+//        GridLayoutManager layoutManager = new GridLayoutManager(mContext, 1, GridLayoutManager.VERTICAL, false);
+        final StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2,
+                StaggeredGridLayoutManager.VERTICAL);
+        photoList.setHasFixedSize(true);
+        photoList.setLayoutManager(layoutManager);
+        photoList.addItemDecoration(new MyDecoration(mContext, MyDecoration.HORIZONTAL_LIST));
+        photoList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
+
+        startAsyncTask();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        startAsyncTask();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        mContext = null;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.d(TAG, "setUserVisibleHint isVisibleToUser: " + isVisibleToUser);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(mContext, "onItemClick: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+        Toast.makeText(mContext, "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void startAsyncTask() {
+
+    }
+
+}
