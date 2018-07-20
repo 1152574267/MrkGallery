ac050d9cf87e6f3746b735af81dbe75e398ef0e7
mrk <1152574267@qq.com>
2018-07-20 13:04:45 +0800

    HW AI Image label detect

--- app/src/main/java/com/mrk/mrkgallery/model/FragmentGenerator.java
+++ app/src/main/java/com/mrk/mrkgallery/model/FragmentGenerator.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentGenerator {
-    public static int[] strArr = new int[]{R.string.tv_image_label_detect, R.string.tv_123};
+    public static int[] strArr = new int[]{R.string.tv_image_scene_detect, R.string.tv_image_label_detect};
     public static int[] drawableArr = new int[]{R.drawable.ic_launcher, R.drawable.ic_launcher};
 
     public static List<Fragment> getFragmentList() {
