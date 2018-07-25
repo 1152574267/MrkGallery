3278b76e3409a4f07f1f6b653e1422e8a9d37f24
mrk <1152574267@qq.com>
2018-07-25 12:43:32 +0800

    HW AI Image label detect

--- app/src/main/java/com/mrk/mrkgallery/tfai/Classifier.java
+++ app/src/main/java/com/mrk/mrkgallery/tfai/Classifier.java
@@ -1,32 +1,20 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 package com.mrk.mrkgallery.tfai;
 
 import android.graphics.Bitmap;
 import android.graphics.RectF;
+
 import java.util.List;
 
 /**
  * Generic interface for interacting with different recognition engines.
  */
 public interface Classifier {
+
     /**
      * An immutable result returned by a Classifier describing what was recognized.
      */
     public class Recognition {
+
         /**
          * A unique identifier for what has been recognized. Specific to the class, not the instance of
          * the object.
@@ -43,11 +31,12 @@ public interface Classifier {
          */
         private final Float confidence;
 
-        /** Optional location within the source image for the location of the recognized object. */
+        /**
+         * Optional location within the source image for the location of the recognized object.
+         */
         private RectF location;
 
-        public Recognition(
-                final String id, final String title, final Float confidence, final RectF location) {
+        public Recognition(final String id, final String title, final Float confidence, final RectF location) {
             this.id = id;
             this.title = title;
             this.confidence = confidence;
@@ -77,18 +66,16 @@ public interface Classifier {
         @Override
         public String toString() {
             String resultString = "";
+
             if (id != null) {
                 resultString += "[" + id + "] ";
             }
-
             if (title != null) {
                 resultString += title + " ";
             }
-
             if (confidence != null) {
                 resultString += String.format("(%.1f%%) ", confidence * 100.0f);
             }
-
             if (location != null) {
                 resultString += location + " ";
             }
